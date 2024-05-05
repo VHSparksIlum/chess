9a3e454c1de75a31e18041bc62cefc36a6dae389
Mark Christensen <mowenc2@gmail.com>
2024-05-04 21:02:06 -0600

    brought over basic chess code from previous project(phase0)

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -1,5 +1,8 @@
 package chess;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 /**
  * A chessboard that can hold and rearrange chess pieces.
  * <p>
@@ -7,7 +10,7 @@ package chess;
  * signature of the existing methods.
  */
 public class ChessBoard {
-    private ChessPiece[][] squares = new ChessPiece[8][8];
+    private ChessPiece[][] board = new ChessPiece[8][8];
     public ChessBoard() {
         
     }
@@ -19,7 +22,7 @@ public class ChessBoard {
      * @param piece    the piece to add
      */
     public void addPiece(ChessPosition position, ChessPiece piece) {
-        squares[position.getRow()][position.getColumn()] = piece;
+        board[position.getRow()][position.getColumn()] = piece;
     }
 
     /**
@@ -30,7 +33,7 @@ public class ChessBoard {
      * position
      */
     public ChessPiece getPiece(ChessPosition position) {
-        return squares[position.getRow()][position.getColumn()];
+        return board[position.getRow()][position.getColumn()];
     }
 
     /**
@@ -38,6 +41,62 @@ public class ChessBoard {
      * (How the game of chess normally starts)
      */
     public void resetBoard() {
-        throw new RuntimeException("Not implemented");
+        this.board = new ChessPiece[8][8];
+        //initializeDefaultBoard();
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ChessBoard that = (ChessBoard) o;
+        return Objects.deepEquals(board, that.board);
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.deepHashCode(board);
+    }
+
+    @Override
+    public String toString() {
+        return "ChessBoard{" +
+                "board=" + Arrays.toString(board) +
+                '}';
+    }
+
+//    private void initializeDefaultBoard() {
+//        // Pawns
+//        for (int col = 1; col <= 8; col++) {
+//            addPiece(new Position(2, col), new Pawn(ChessGame.TeamColor.WHITE));
+//            addPiece(new Position(7, col), new Pawn(ChessGame.TeamColor.BLACK));
+//        }
+//
+//        // Rooks
+//        addPiece(new Position(1, 'a' - 'a' + 1), whiteQueenSideRook);
+//        addPiece(new Position(1, 'h' - 'a' + 1), whiteKingSideRook);
+//        addPiece(new Position(8, 'a' - 'a' + 1), blackQueenSideRook);
+//        addPiece(new Position(8, 'h' - 'a' + 1), blackKingSideRook);
+//
+//        // Knights
+//        addPiece(new Position(1, 'b' - 'a' + 1), new Knight(ChessGame.TeamColor.WHITE));
+//        addPiece(new Position(1, 'g' - 'a' + 1), new Knight(ChessGame.TeamColor.WHITE));
+//        addPiece(new Position(8, 'b' - 'a' + 1), new Knight(ChessGame.TeamColor.BLACK));
+//        addPiece(new Position(8, 'g' - 'a' + 1), new Knight(ChessGame.TeamColor.BLACK));
+//
+//        // Bishops
+//        addPiece(new Position(1, 'c' - 'a' + 1), new Bishop(ChessGame.TeamColor.WHITE));
+//        addPiece(new Position(1, 'f' - 'a' + 1), new Bishop(ChessGame.TeamColor.WHITE));
+//        addPiece(new Position(8, 'c' - 'a' + 1), new Bishop(ChessGame.TeamColor.BLACK));
+//        addPiece(new Position(8, 'f' - 'a' + 1), new Bishop(ChessGame.TeamColor.BLACK));
+//
+//        // Queens
+//        addPiece(new Position(1, 'd' - 'a' + 1), new Queen(ChessGame.TeamColor.WHITE));
+//        addPiece(new Position(8, 'd' - 'a' + 1), new Queen(ChessGame.TeamColor.BLACK));
+//
+//        // Kings
+//        addPiece(new Position(1, 'e' - 'a' + 1), new King(ChessGame.TeamColor.WHITE));
+//        addPiece(new Position(8, 'e' - 'a' + 1), new King(ChessGame.TeamColor.BLACK));
+//
+//    }
 }
--- shared/src/main/java/chess/ChessMove.java
+++ shared/src/main/java/chess/ChessMove.java
@@ -1,5 +1,7 @@
 package chess;
 
+import java.util.Objects;
+
 /**
  * Represents moving a chess piece on a chessboard
  * <p>
@@ -8,22 +10,50 @@ package chess;
  */
 public class ChessMove {
 
-    public ChessMove(ChessPosition startPosition, ChessPosition endPosition,
-                     ChessPiece.PieceType promotionPiece) {
+    private ChessPosition startPosition;
+    private ChessPosition endPosition;
+    private ChessPiece.PieceType promotionPiece;
+    private boolean isEnPassant;
+
+    /**
+     * Constructor for regular moves (without promotion))
+     */
+    public ChessMove(ChessPosition startPosition, ChessPosition endPosition) {
+        this.startPosition = startPosition;
+        this.endPosition = endPosition;
+        this.promotionPiece = null;
     }
 
+    /**
+     * Constructor for moves with promotion
+     */
+    public ChessMove(ChessPosition startPosition, ChessPosition endPosition, ChessPiece.PieceType promotionPiece) {
+        this.startPosition = startPosition;
+        this.endPosition = endPosition;
+        this.promotionPiece = promotionPiece;
+    }
+
+    /**
+     * Constructor for moves with En Passant
+     */
+//    public ChessMove(ChessPosition start, ChessPosition end, ChessPiece.PieceType promotionPiece, boolean isEnPassant) {
+//        this.startPosition = start;
+//        this.endPosition = end;
+//        this.promotionPiece = promotionPiece;
+//        this.isEnPassant = isEnPassant;
+//    }
     /**
      * @return ChessPosition of starting location
      */
     public ChessPosition getStartPosition() {
-        throw new RuntimeException("Not implemented");
+        return this.startPosition;
     }
 
     /**
      * @return ChessPosition of ending location
      */
     public ChessPosition getEndPosition() {
-        throw new RuntimeException("Not implemented");
+        return this.endPosition;
     }
 
     /**
@@ -33,6 +63,74 @@ public class ChessMove {
      * @return Type of piece to promote a pawn to, or null if no promotion
      */
     public ChessPiece.PieceType getPromotionPiece() {
-        throw new RuntimeException("Not implemented");
+        return this.promotionPiece;
+    }
+
+//    /**
+//     * Set the starting position of this chess move
+//     *
+//     * @param startPosition the ChessPosition to set as the starting position
+//     */
+//    public void setStartPosition(ChessPosition startPosition) {
+//        this.startPosition = startPosition;
+//    }
+//
+//    /**
+//     * Set the ending position of this chess move
+//     *
+//     * @param endPosition the ChessPosition to set as the ending position
+//     */
+//    public void setEndPosition(ChessPosition endPosition) {
+//        this.endPosition = endPosition;
+//    }
+//
+//    /**
+//     * Set the promotion piece for this chess move
+//     *
+//     * @param promotionPiece the ChessPiece.PieceType to set as the promotion piece
+//     */
+//    public void setPromotionPiece(ChessPiece.PieceType promotionPiece) {
+//        this.promotionPiece = promotionPiece;
+//    }
+//
+//    /**
+//     * Apply pawn promotion by setting the promotion piece
+//     *
+//     * @param promotionPiece the ChessPiece.PieceType to set as the promotion piece
+//     */
+//    public void applyPromotion(ChessPiece.PieceType promotionPiece) {
+//        this.promotionPiece = promotionPiece;
+//    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("Move from ");
+        sb.append(startPosition.toString()); // Use the custom toString for Position
+        sb.append(" to ");
+        sb.append(endPosition.toString()); // Use the custom toString for Position
+
+//        if (promotionPiece != null) {
+//            sb.append(" promoting to ");
+//            sb.append(promotionPiece.toString()); // Use the custom toString for PieceType
+//        }
+
+        return sb.toString();
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ChessMove chessMove = (ChessMove) o;
+        if (promotionPiece != chessMove.promotionPiece) return false; // Explicitly compare promotionPiece
+        return Objects.equals(startPosition, chessMove.startPosition) && Objects.equals(endPosition, chessMove.endPosition);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(startPosition, endPosition, promotionPiece);
     }
 }
